c4330fb49d72bccf839b1bd4fa5fab9d0c2aaf3a
Sóla Lusøt <60041069+solaluset@users.noreply.github.com>
2024-08-08 19:05:26 +0300

    Add apostrophe support (#4)
    * Add apostrophe support
    
    * Add apostrophe test
    
    * Bump version to 0.0.2
    
    * Improve apostrophe handling
    
    * Update apostrophe test
    
    * Use black formatter
    
    * Add pre-commit

--- hadiuka/runner.py
+++ hadiuka/runner.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 from importlib import metadata
+from collections import defaultdict
 
 import pwcp
 
@@ -10,6 +11,17 @@ from .translator import mapping, string_modifiers, translate
 
 
 __version__ = metadata.version(__package__)
+
+mapping_with_apostrophes = defaultdict(list)
+for k, v in mapping.items():
+    first_part, *rest = k.split("'")
+    if rest:
+        mapping_with_apostrophes[first_part].append((k, v))
+        mapping_with_apostrophes[first_part].sort(
+            key=lambda elem: len(elem[0]), reverse=True
+        )
+    else:
+        mapping_with_apostrophes[first_part] = v
 
 parser = argparse.ArgumentParser(
     (
@@ -22,7 +34,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "--version", action="version", version=__package__ + " " + __version__
 )
-parser.add_argument("-m", action="store_true", help="запустити target як модуль")
+parser.add_argument(
+    "-m", action="store_true", help="запустити target як модуль"
+)
 parser.add_argument("target", nargs=argparse.OPTIONAL)
 parser.add_argument("args", nargs=argparse.ZERO_OR_MORE)
 
@@ -36,7 +50,7 @@ def main(args=sys.argv[1:]):
         preprocessor.disabled = True
         return translate(
             orig_preprocess(src, filename, preprocessor),
-            mapping,
+            mapping_with_apostrophes,
             string_modifiers,
         )
 
--- hadiuka/translator.py
+++ hadiuka/translator.py
@@ -4,7 +4,9 @@ import json
 from pypp.parser import default_lexer
 
 
-with open(os.path.join(os.path.dirname(__file__), "mapping.json")) as mapping_file:
+with open(
+    os.path.join(os.path.dirname(__file__), "mapping.json")
+) as mapping_file:
     mapping = json.load(mapping_file)
 
 string_modifiers = {
@@ -13,7 +15,9 @@ string_modifiers = {
     "с": "r",
     "ю": "u",
 }
-string_modifiers.update({k.upper(): v.upper() for k, v in string_modifiers.items()})
+string_modifiers.update(
+    {k.upper(): v.upper() for k, v in string_modifiers.items()}
+)
 
 
 def _translate_gen(src, word_mapping, letter_mapping):
@@ -22,7 +26,43 @@ def _translate_gen(src, word_mapping, letter_mapping):
     while tok := lex.token():
         value = tok.value
         if value in word_mapping:
-            yield word_mapping[value]
+            result = word_mapping[value]
+            if isinstance(result, str):
+                yield result
+            else:
+                next_tok = lex.token()
+                if not next_tok:
+                    return
+                next_value = next_tok.value
+                if not next_value.startswith("'"):
+                    yield value
+                    yield next_value
+                else:
+                    next_value = value + next_value
+                    while not (
+                        match := next(
+                            (
+                                (word, replacement)
+                                for word, replacement in result
+                                if next_value.startswith(word)
+                            ),
+                            None,
+                        )
+                    ):
+                        next_tok = lex.token()
+                        if not next_tok:
+                            return
+                        next_value += next_tok.value
+                    yield match[1]
+                    rest = ""
+                    while tok := lex.token():
+                        rest += tok.value
+                    yield from _translate_gen(
+                        next_value[len(match[0]) :] + rest,
+                        word_mapping,
+                        letter_mapping,
+                    )
+                    return
         elif set(value) <= set(letter_mapping):
             yield "".join(letter_mapping[c] for c in value)
         else:
